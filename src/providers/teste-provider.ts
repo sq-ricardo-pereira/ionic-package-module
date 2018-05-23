9f4ba1a52ebb8960e528603b12aeb2ad13383b7a
Ricardo Pereira <rialexsandro@gmail.com>
2018-05-23 16:47:47 -0300

    teste

--- src/providers/teste-provider.ts
+++ src/providers/teste-provider.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TesteProvider {
   reasonToJoin() {
-    return 'Teste de plugin ionic!';
+    return 'Teste de plugin ionic!'; 
   }
 }
